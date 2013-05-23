8f4ec8c7258098782275ac042709fa9bc1da8449
max360se <marino.zic@kvarner-punat.hr>
2013-05-23 15:05:46 +0200

    dodano nesto sitno, jos provjeriti ne radi kako treba, vjerojatno problem u input bufferu

--- v44.c
+++ v44.c
@@ -4,7 +4,6 @@ http://psihijatrija.forumhr.com/t217-test-osobnosti
 */
 
 #include <stdio.h>
-#include <ncurses.h>
 
 main()
 {
@@ -15,7 +14,7 @@ main()
 	
 	puts ("Kad se osjecate najbolje?\na. Ujutro (2 boda)\nb. Poslije podne i prevecer (4 boda)\nc. Kasno navecer (6 bodova)\n");
 
-	odg_1 = getch();
+	odg_1 = getchar();
 	printf("Unijeli ste %c\n_________________________________\n",odg_1);
 
 	switch(odg_1){
@@ -25,7 +24,7 @@ main()
 		case 'b':
 			ukupno+=4;
 				break;
-		case 'c';
+		case 'c':
 			ukupno+=6;
 				break;
 	}
@@ -178,10 +177,51 @@ main()
                 break;
 	}
 
-	puts("10. Često sanjate da...
-a. Padate (4 boda)
-b. Se tučete ili borite (2 boda)
-c. Nešto ili nekoga tražite (3 boda)
-d. Da letite ili lebdite (5 bodova)
-e. Uglavnom ne sanjate (6 bodova)
-f. Snovi su vam uvijek ugodni (1 bod)
+	puts("10. Često sanjate da...\na. Padate (4 boda)\nb. Se tučete ili borite (2 boda)\nc. Nešto ili nekoga tražite (3 boda)\nd. Da letite ili lebdite (5 bodova)\ne. Uglavnom ne sanjate (6 bodova)\nf. Snovi su vam uvijek ugodni (1 bod)\n");
+
+	switch(odg_10){
+		case 'a':
+            ukupno+=4;
+                break;
+        case 'b':
+            ukupno+=2;
+                break;
+        case 'c':
+            ukupno+=3;
+                break;
+        case 'd':
+            ukupno+=5;
+                break;
+        case 'e':
+            ukupno+=6;
+                break;
+        case 'f':
+            ukupno+=1;
+                break;
+	}
+
+	if (ukupno > 60){
+		printf("DOMINANTNOST\nDrugi vas vide kao osobu s kojojm treba ''pažljivo postupati'', kao oholu, egocentričnu i izrazito dominantnu osobu. Neki vam se dive i žele biti poput vas, ali vam ne vjeruju uvijek i oklijevaju stupiti s vama u neki dublji odnos.\n");
+	}
+	
+	else if (ukupno > 50){
+		printf("TEMPERAMENTNOST\nDrugi vas vide kao uzbudljivu osobu, eksplozivnog temperamenta, impulzivnu, kao prirodnog vođu koji brzo donosi odluke, iako ne uvijek ispravne. Smatraju vas hrabrim, avanturistički nastrojenim, kao nekoga tko će se uvijek usuditi pokušati i tko uživa u avanturi. Uživaju biti u vašem društvu zbog uzbudljivosti kojom zračite.\n");
+	}
+
+	else if (ukupno > 40){
+		printf("PRAKTIČNOST\nDrugi vas vide kao direktnu, živahnu, šarmantnu, zabavnu, praktičnu i uvijek zanimljivu osobu koja je uvijek u centru pažnje, ali koja je dovoljno uravnotežena da joj to ne udari u glavu. Također vas smatraju ljubaznom, obzirnom osobom punom razumijevanja koja će uvijek razveseliti i pomoći.\n");
+	}
+
+	else if (ukupno > 30){
+		printf("ODANOST\nDrugi vas vide kao razumnu, opreznu, brižnu osobu, pametnu, talentiranu, ali skromnu. Niste osoba koja prebrzo sklapa prijateljstva, ali ste izrazito odani onima koji postanu vašim prijateljima i za uzvrat očekujete istu odanost. Oni koji vas stvarno upoznaju, shvaćaju da je potrebno uistinu puno da se vaša vjera u prijatelja pokoleba, ali vam isto tako treba puno da zaboravite, ako se to uistinu i dogodi.\n");
+	}
+
+	else if (ukupno > 20){
+		printf("OPREZNOST\nPrijatelji vas smatraju savjesnom i pedantnom osobom. Vide vas kao vrlo opreznu, izrazito brižnu osobu, kao nekoga tko polagano i ustrajno radi. Uistinu bi ih iznenadilo da iznenada reagirate impulzivno, budući da očekuju od vas da sve ispitujete pažljivo iz svakog ugla i tek onda odustanete. Smatraju da je razlog tome vaša oprezna priroda.\n");
+	}
+
+	else {
+		printf("NEODLUČNOST\nLjudi vas smatraju sramežljivom, nervoznom, neodlučnom osobom, za koju se potrebno brinuti, koja uvijek želi da netko drugi donese odluku i koja se ne želi ni u što miješati. Vi ste za njih vječno uznemirena osoba koja samo vidi nepostojeće probleme. Neki ljudi vas smatraju dosadnima. Samo oni koji vas dobro poznaju znaju da to niste.\n");
+	}
+}
+
